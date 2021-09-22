17f0ec913b1352c2d2ac8a2feabc0196bdb9dfb7
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2021-09-22 08:58:52 +0200

    Add project files.

---
+++ Prb.ClassesAndObjects.Core/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prb.ClassesAndObjects.Core
+{
+    public class Car 
+    {
+        private int topSpeed;
+
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public int TopSpeed
+        {
+            get { return topSpeed; }
+            set
+            {
+                if (value < 0)
+                    topSpeed = 0;
+                else
+                    topSpeed = value;
+            }
+        }
+
+        public Car()  
+        {
+        }
+    }
+
+}
---
+++ Prb.ClassesAndObjects.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Prb.ClassesAndObjects.Core;
+
+namespace Prb.ClassesAndObjects.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Car> cars;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cars = new List<Car>();
+        }
+
+        private void BtnAddNewCar_Click(object sender, RoutedEventArgs e)
+        {
+            string color = txtColor.Text.Trim();
+            string carbrand = txtCarBrand.Text.Trim();
+            decimal.TryParse(txtPrice.Text.Trim(), out decimal price);
+            int.TryParse(txtTopSpeed.Text.Trim(), out int topSpeed);
+            Car car = new Car();
+            car.Color = color;
+            car.Brand = carbrand;
+            car.Price = price;
+            car.TopSpeed = topSpeed;
+            cars.Add(car);
+            UpdateCarListbox();
+        }
+        private void UpdateCarListbox()
+        {
+            lstCars.ItemsSource = cars;
+            lstCars.Items.Refresh();
+        }
+
+        private void LstCars_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstCars.SelectedItem != null)
+            {
+                Car car = (Car)lstCars.SelectedItem;
+                txtCarBrand.Text = car.Brand;
+                txtColor.Text = car.Color;
+                txtPrice.Text = car.Price.ToString();
+                txtTopSpeed.Text = car.TopSpeed.ToString();
+            }
+        }
+    }
+}
